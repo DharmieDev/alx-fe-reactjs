e316fa27e6d49484fb03208652f850f348bc7831
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-07-18 22:50:14 +0100

    Third React app

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import './App.css';
-import { MyContext } from './UserContext.js';
-import ProfilePage from './components/ProfilePage';
-
+import React from "react";
+import "./App.css";
+import { MyContext } from "./components/UserContext.js";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
-  
-  const userData = {name: "Jane Doe", email: "jane.doe@example.com"};
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    
     <MyContext.Provider value={userData}>
-     <ProfilePage  />
-    </MyContext.Provider> 
-  )
+      <ProfilePage />
+    </MyContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,15 +1,15 @@
 import { useContext } from "react";
-import { MyContext } from "../UserContext";
+import { MyContext } from "./UserContext";
 
 function UserDetails() {
-    const userData = useContext(MyContext);
-    
+  const userData = useContext(MyContext);
+
   return (
     <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
-  )
+  );
 }
 
-export default UserDetails
+export default UserDetails;
