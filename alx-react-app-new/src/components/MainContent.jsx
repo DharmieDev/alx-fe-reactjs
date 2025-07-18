4f4117624b813c404d6396cbe5ff78bb76a7e610
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-07-18 20:07:40 +0100

    Second React app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main style={{
+      padding: "20px",
+      border: "1px solid gray",
+      color: "black",
+      textAlign: "center",
+      borderRadius: "10px",
+    }}>
+        <p style={{fontSize: "24px"}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+
+export default MainContent
