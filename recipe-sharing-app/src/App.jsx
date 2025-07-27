bf937fd81708e9d70462993f0afc0e6373292c84
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-07-27 06:02:41 +0100

    Recipe sharing React app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       <RecipeList />
+       <AddRecipeForm />
+      </div>
+    </>
+  )
+}
+
+export default App
