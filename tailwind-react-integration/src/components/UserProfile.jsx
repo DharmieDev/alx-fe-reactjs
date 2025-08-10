f21ee70fc372caea1cbc787db77d2c4e235504d2
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-08-10 08:45:56 +0100

    Second update to component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-xl sm:p-4 sm:max-w-xs md:p-8 md:max-w-sm">
       <img src="https://avatar.iran.liara.run/public/48" alt="User" className="rounded-full w-36 h-36 mx-auto md:w-36 md:h-36 sm:w-24 sm:h-24"/>
-      <h1 className="text-xl text-blue-800 hover:text-blue-500 my-4 md:text-x1 sm:text-lg">John Doe</h1>
+      <h1 className="text-xl text-blue-800 hover:text-blue-500 my-4 md:text-xl sm:text-lg">John Doe</h1>
       <p className="text-gray-600 text-base md:text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
