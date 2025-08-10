ae4a37264adffeda44845734ce413eca72a801b7
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-08-10 08:18:30 +0100

    First update to the app

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
 
   return (
     <>
-     <h1 className='text-5xl font-bold'>My React with Tailwind</h1> 
+     <h1 className='text-5xl font-bold'>My React with Tailwind</h1>
+     <UserProfile /> 
     </>
   )
 }
