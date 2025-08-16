3986aff02345b8a80079dfcae2f0136c75db8a27
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-08-16 22:49:33 +0100

    Second update added

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Recipes from '../data.json'
+import { Link } from 'react-router-dom'
 
 
 const HomePage = () => {
@@ -9,28 +10,30 @@ const HomePage = () => {
     // To load data when the component mounts
     useEffect(() => {
         // fetch data from the JSON file
-        {setRecipes(Recipes)}
+        setRecipes(Recipes)
     }, [])
 
 
   return (
       <div className='bg-gray-100'>
-          <div className='m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          <div className='m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
               {recipes.map((recipe) => (
-                <div key={recipe.id}
-                    className='block w-72 h-auto rounded-xl bg-white overflow-hidden shadow-md
-                    hover:shadow-xl transition-shadow duration-300 transform hover:scale-105'
-                    >
-                  <img
-                  src={recipe.image}
-                  alt={recipe.title}
-                  className='w-72 h-48 object-cover'
-                  />
-                  <div className='m-2 p-2'>
-                    <h3 className='font-semibold mb-1'>{recipe.title}</h3>
-                    <p className='text-sm text-gray-500'>{recipe.summary}</p>
-                  </div>
-              </div>
+                <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
+                    <div key={recipe.id}
+                        className='block w-60 h-auto rounded-xl bg-white overflow-hidden shadow-md
+                        hover:shadow-xl transition-shadow duration-300 transform hover:scale-105'
+                        >
+                      <img
+                      src={recipe.image}
+                      alt={recipe.title}
+                      className='w-60 h-48 object-cover'
+                      />
+                      <div className='m-2 p-2'>
+                        <h3 className='font-semibold mb-1'>{recipe.title}</h3>
+                        <p className='text-sm text-gray-500'>{recipe.summary}</p>
+                      </div>
+                    </div>
+                </Link>
               ))}
           </div>
       </div>
