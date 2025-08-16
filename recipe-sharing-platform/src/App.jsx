564189595133ae62986b2e016b6802a8266c0ee8
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-08-16 22:48:01 +0100

    First update added

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './components/HomePage'
+import RecipeDetail from './components/RecipeDetail'
+import SideBar from './components/SideBar'
 
 function App() {
 
   return (
-    <>
-      <h1 className="font-Mozilla text-blue-700">Recipe Sharing platform</h1>
-    </>
+    <Router>
+      <div className="block md:grid grid-cols-[200px_1fr]">
+        <SideBar />
+        <div>
+          <Routes>
+            <Route path='/' element={<HomePage />}/>
+            <Route path='/recipe/:id' element={<RecipeDetail />}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 
