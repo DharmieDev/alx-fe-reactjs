e18b861be2ddec2c4ada7e9a683ad4516c638eb7
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-08-17 09:50:34 +0100

    Fourth update added

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import HomePage from './components/HomePage'
 import RecipeDetail from './components/RecipeDetail'
 import SideBar from './components/SideBar'
+import AddRecipeForm from './components/AddRecipeForm'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path='/' element={<HomePage />}/>
             <Route path='/recipe/:id' element={<RecipeDetail />}/>
+            <Route path='/add-recipe' element={<AddRecipeForm />} />
           </Routes>
         </div>
       </div>
