cf0036e355b109b93d2b967aa57799be6cc06ca6
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-08-24 11:04:31 +0100

    React query demo app

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import PostsComponent from './components/PostsComponents'
+import './App.css'
+
+const queryClient = new QueryClient();
+
+function App() {
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default App
